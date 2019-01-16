8b10ad08b6e2dfc543db6c76f36452001ba813f9
baiyuting <baiyuting_hi@163.com>
2019-01-16 17:14:17 +0800

    文本修改中

---
+++ error_word/detect_error_model_pycorrector.py
@@ -0,0 +1,10 @@
+"""
+pycorrector 工具使用
+"""
+from pycorrector.tokenizer import Tokenizer
+
+
+# class pycorrector_token(Tokenizer):
+#
+#     def tokenize(self, sentence):
+#        return list(jieba.cut)
--- error_word/test/cut_word.py
+++ error_word/test/cut_word.py
@@ -1,23 +1,43 @@
 import jieba
 
 import pycorrector
+import re
 
 # text = '少先队员因该为老人让坐'
 f = open('文本.txt', "r", encoding="utf-8")
 lines = f.readlines()
 f.close()
 
-text = "".join(lines)
-idx_errors = pycorrector.detect(text)
-characters = []
-for c in text:
-    characters.append(c)
 
-for idx_error in idx_errors:
-    end = idx_error[2]  # [词、位置、错误类型] [['因该', 4, 6, 2], ['坐', 10, 11, 3]]
-    characters[end - 1] = characters[end - 1] + "（True）"
+def detect_sentence(text):
+    """
+    检查句子错误
+    :param text:
+    :return:
+    """
+    idx_errors = pycorrector.detect(text)
+    characters = []
+    for c in text:
+        characters.append(c)
+    for idx_error in idx_errors:
+        end = idx_error[2]  # [词、位置、错误类型] [['因该', 4, 6, 2], ['坐', 10, 11, 3]]
+        characters[end - 1] = characters[end - 1] + "（True）"
+    text = "".join(characters)
+    return text
 
-text = "".join(characters)
+
+ts = []
+for line in lines:
+    segs = re.split("[\.\?]", line)
+    for seg in segs:
+        punctuation_i = line.index(seg) + len(seg)
+        punctuation = ""
+        if punctuation_i < len(line):
+            punctuation = line[punctuation_i]
+        res = detect_sentence(seg) + punctuation
+        ts.append(res)
+
+text = "".join(ts)
 f = open("文本-括号中是pycorrector修改的.txt", "w", encoding="utf-8")
 f.writelines(text)
 f.close()
