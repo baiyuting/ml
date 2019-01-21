c3c8f4b9e5ef485d05dc88d3d105f19cfe0cb9f6
baiyuting <baiyuting_hi@163.com>
2019-01-21 14:07:55 +0800

    文本修改中

--- error_word/detect_error.py
+++ error_word/detect_error.py
@@ -134,6 +134,41 @@ def K_3(i, segments):
     return K3
 
 
+def two_words_model_frequency(i, segments):
+    """
+    二元词 接续模型 频次统计 [f1,f2]
+    :param i:
+    :param segments:
+    :return:
+    """
+    return word_model_frequency(i, segments, 2)
+
+
+def three_words_model_frequency(i, segments):
+    """
+    三元词 接续模型 频次统计 [f1,f2,f3]
+    :param i:
+    :param segments:
+    :return:
+    """
+    return word_model_frequency(i, segments, 3)
+
+
+def word_model_frequency(i, segments, n_gram):
+    """
+    通用 单词接续模型 频次 统计
+    :param i:
+    :param segments:
+    :return:
+    """
+    count = [0 for _ in range(n_gram)]
+    begin, end = word_scope(i, len(segments), n_gram)
+    for index in range(begin, end - n_gram + 2):  # [begin, end-n_gram+1] 或者 [begin, end-n_gram+2)
+        words = [segments[i] for i in range(index, index + n_gram)]
+        count[index] = R(words, 0)
+    return count
+
+
 def character_scope(c_i, w_len, s_len, n_gram):
     """
     字的范围 [begin, end]
@@ -210,6 +245,48 @@ def K_5(i, c_i, segments, text):
     return K5
 
 
+def two_characters_model_frequency(i, c_i, segments, text):
+    """
+    二元字模型 频次 统计 [f1,f2]
+    :param i:
+    :param c_i:
+    :param segments:
+    :param text:
+    :return: [x,x]
+    """
+    return character_model_frequency(i, c_i, segments, text, 2)
+
+
+def three_characters_model_frequency(i, c_i, segments, text):
+    """
+    三元字模型 频次 统计 [f1,f2,f3]
+    :param i:
+    :param c_i:
+    :param segments:
+    :param text:
+    :return:
+    """
+    return character_model_frequency(i, c_i, segments, text, 3)
+
+
+def character_model_frequency(i, c_i, segments, text, n_gram):
+    """
+    通用的 字模型 频次统计
+    :param i:
+    :param c_i:
+    :param segments:
+    :param text:
+    :param n_gram:
+    :return:
+    """
+    count = [0 for _ in range(n_gram)]
+    begin, end = character_scope(c_i, len(segments[i]), len(text), n_gram)
+    for index in range(begin, end - (n_gram - 1) + 1):  # [begin, end-n_gram+1]
+        chars = [text[i] for i in range(index, index + n_gram)]
+        count[index] = R(chars, 1)
+    return count
+
+
 def detect_error(segments, text):
     """
     查错
@@ -221,14 +298,13 @@ def detect_error(segments, text):
     c_i = 0
     for i in range(len(segments)):
         error_i = False  # 默认这个位置是没有错的
-        K1, K2, K3, K4, K5 = K_1(i, segments), K_2(i, segments), K_3(i, segments), K_4(i, c_i, segments, text), K_5(i,
-                                                                                                                    c_i,
-                                                                                                                    segments,
-                                                                                                                    text)
+        K1, K2, K3, K4, K5 = K_1(i, segments), \
+                             K_2(i, segments), K_3(i, segments), \
+                             K_4(i, c_i, segments, text), K_5(i, c_i, segments, text)
         c_i += len(segments[i])
         # K1 = 0
-        K2 = 0
-        K3 = 0
+        # K2 = 0
+        # K3 = 0
         # K4 = 0
         # K5 = 0
         K = K1 + K2 + K3 + K4 + K5
